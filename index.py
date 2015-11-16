d84eecda0710bbd2af2340057dd7dd0eb8a1322d
Manraj Singh <manrajsinghgrover@gmail.com>
2015-11-16 18:57:34 +0530

    Table extracted

--- index.py
+++ index.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+import urllib
+import json
 app = Flask(__name__)
 
+@app.route('/api/<cricketer>',methods=['GET'])
+def api(cricketer=None):
+    res = {}
+    if cricketer == None:
+        res['error'] = True
+        res['message'] = 'Please provide a cricketer name as GET parameter'
+        res = json.dumps(res)
+    else:
+        cricketer = cricketer.replace (" ", "_")
+        url = 'https://en.wikipedia.org/wiki/'+str(cricketer)
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html)
+        soup = soup.find("table", {"class": "infobox vcard"})
+    return res
+
 @app.route('/')
-@app.route('/index/')
+@app.route('/index')
 def index():
     return 'Welcome to Crick Info API, this is currently under development!'
 
