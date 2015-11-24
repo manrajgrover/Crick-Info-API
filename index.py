ccfd6ad892849c2536eb72c53f49a6a3e8e3b8d6
Manraj Singh <manrajsinghgrover@gmail.com>
2015-11-25 00:39:00 +0530

    Base API created

--- index.py
+++ index.py
@@ -2,8 +2,19 @@ from flask import Flask
 from flask import jsonify
 from bs4 import BeautifulSoup
 import urllib
+import re
 import json
+import unicodedata
 app = Flask(__name__)
+
+def remove_tags(text):
+    return ''.join(xml.etree.ElementTree.fromstring(text).itertext())
+
+def remove_brackets(text):
+    ret = re.sub('\[.+?\]', '', text)
+    #ret = re.sub('[()]', '', ret)
+    ret = re.sub('\(.+?\)','', ret)
+    return ret
 
 @app.route('/api/')
 @app.route('/api/<cricketer>')
@@ -15,14 +26,28 @@ def api(cricketer=None):
         return jsonify(res)
     else:
         res = {}
-        try:
-            cricketer = cricketer.replace (" ", "_")
-            url = 'https://en.wikipedia.org/wiki/'+str(cricketer)
-            html = urllib.urlopen(url).read()
-            soup = BeautifulSoup(html)
-            soup = soup.find("table", {"class": "infobox vcard"})
-        except:
-            return flask.response()
+        cricketer = cricketer.replace (" ", "_")
+        url = 'https://en.wikipedia.org/wiki/'+str(cricketer)
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html)
+        current = None
+        for row in soup.find("table", {"class": "infobox vcard"}).findAll('tr'):
+            children = row.find_all(True, recursive=False)
+            if len(children) == 1:
+                if children[0].name == 'th':
+                    current = unicodedata.normalize('NFKD',children[0].text).encode('ascii','ignore')
+                    current = current.lower().replace(" ","_")
+                    res[current] = {}
+                elif children[0].name == 'td' and children[0].table:
+                    print "Get table here!"
+            elif len(children) == 2:
+                if current is not None:
+                    value = unicodedata.normalize('NFKD',children[1].text).encode('ascii','ignore')
+                    key = unicodedata.normalize('NFKD',children[0].text).encode('ascii','ignore')
+                    key = remove_brackets(key).lower().replace(" ","_").replace(".","")
+                    value = remove_brackets(value)
+                    res[current][key] = value
+        print res
         return jsonify(res)
 
 @app.route('/')
